2c78857b2185b4577687cca59012ee41fd5f1579
Léo Maradan <leomaradan@gmail.com>
2015-01-12 13:04:11 +0100

    Initial commit
    Initial commit of the project

---
+++ app/lang/fr/home.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    "presentation" => [
+    	"title" => "GameFramework",
+    	"text" => [
+    		"GameFramework est un outil d'aide au développement de jeux php.",
+    		"Il utilise et étends le framework Laravel"
+    	],
+    ]
+
+
+
+);
